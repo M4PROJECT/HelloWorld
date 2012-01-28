1065461b24c719fbdcf4315b21da145da0584018
M4PROJECT <chongssssssch@gmail.com>
2012-01-28 13:40:58 +0700

    First Commit

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -7,12 +7,49 @@
 //
 
 #include <iostream>
+#include <string>
+
+class Person
+{
+    private:
+        char* m_name;
+        char* m_gender;
+        int m_age;
+    public:
+        Person()
+    {
+        m_name = "unknown";
+        m_gender = "unknown";
+        m_age = 1;
+    }
+        Person(char* Name, char* Gender, int Age)
+    {
+        m_name = Name;
+        m_gender = Gender;
+        m_age = Age;
+    }
+        void Show()
+    {
+        std::cout << "Person info:\n";
+        std::cout << m_name << "\n";
+        std::cout << m_gender << "\n";
+        std::cout << m_age << "\n";
+    }
+};
 
 int main (int argc, const char * argv[])
 {
 
     // insert code here...
+    std::cout << "hello, first";
     std::cout << "Hello, World!\n";
+    std::cout << "This is person object\n";
+    Person p1;
+    Person p2("Chong","Male",22);
+    std::cout << "This is Person 1 info:\n";
+    p1.Show();
+    std::cout << "This is Person 2 info:\n";
+    p2.Show();
     return 0;
 }
 
